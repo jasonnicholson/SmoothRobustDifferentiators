3f40744161e558b56476fb84ed2f1d120e8ef9c8
Jason Nicholson <jashale@yahoo.com>
2014-03-22 15:22:20 -0500

    Documentation updates

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
-% ROBUSTDIFFERENTIATORS
-% Version 1.1   03-03-2014
+% Smooth Robust Differentiators
+% Version 1.1   03-22-2014
 % Files
 %   robustDiff - differentiates using smooth noise-robust differentiation formula
 %   robustDiffOneSide - differentiates using smooth noise-robust one sided differentiation formula
