1e642ae05cbb989a72fd3f77f99114a6de6e91cd
Jason Nicholson <jashale@yahoo.com>
2014-03-22 15:09:13 -0500

    Updates to documentation

--- Documentation/documentation generators/generateDocumentation.m
+++ Documentation/documentation generators/generateDocumentation.m
@@ -1,3 +1,7 @@
 publish('index.m','outputDir','..\');
 publish('robustDiffDocumentation.m','outputDir','..\');
 publish('robustDiffOneSideDocumentation.m','outputDir','..\');
+
+directory = which('generateDocumentation.m');
+documentationDirectory = regexprep(directory,['\' filesep 'documentation generators' '\' filesep 'generateDocumentation.m'],'');
+builddocsearchdb(documentationDirectory);
